782311e0517b34d8243e510ae3440c8529d47a31
giovanni <giovanni.girelli90@gmail.com>
2019-02-23 00:56:44 +0100

    More comments

--- pipe.py
+++ pipe.py
@@ -8,16 +8,26 @@ class Pipe:
         self.computation = []
 
     def __rshift__(self, next_function):
+        """Pipes a function. The will be executed in the order they are piped,
+        left to right
+         p = Pipe(a,b) >> f >> g >> h ---> h(g(f(a,b)))
+        """
         self.computation.append(next_function)
         return self
 
     def __call__(self,*args):
+        """
+        calling the pipe will execute. You can pass more arguments here.
+        """
         g = self.computation.pop()(*self.args , *args)
         for f in self.computation:
             g = f(g)
         return g
 
     def __lshift__(self, other):
+        """
+        pipe arguments in. They are appended after whatever you just passed
+        """
         #self.args = self.args + (other,) if len(self.args) > 0 else (other,)
         self.args = (*self.args,other)
         return self.__call__()
