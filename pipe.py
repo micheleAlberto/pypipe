cf2917d1f7c5ea9a75c1a0957b75111fda6bc570
giovanni <giovanni.girelli90@gmail.com>
2019-02-24 11:37:45 +0100

    Added placeholder argument

--- pipe.py
+++ pipe.py
@@ -1,3 +1,6 @@
+class PlaceHolderArg:
+    pass
+
 
 class Pipe:
     def __init__(self,*args):
@@ -20,7 +23,7 @@ class Pipe:
         """
         calling the pipe will execute. You can pass more arguments here.
         """
-        g = self.computation.pop()(*self.args , *args)
+        g = self.computation.pop(0)(*self.args , *args)
         for f in self.computation:
             g = f(g)
         return g
@@ -31,10 +34,42 @@ class Pipe:
         Multiple arguments can be passed as a tuple.
 
         """
-        #self.args = self.args + (other,) if len(self.args) > 0 else (other,)
         if isinstance(other, tuple):
-            self.args = (*self.args,*other )
+            other = list(other)
+            args = list(self.args)
+            for k,i in enumerate(args):
+                if isinstance(i, PlaceHolderArg):
+                    arg = other.pop(0)
+                    args[k] = arg
+                else:
+                    continue
+            self.args = (*args,*other)
         else:
-            self.args = (*self.args,other )
+            for k,i in enumerate(self.args):
+                if isinstance(i, PlaceHolderArg):
+                    args = list(self.args)
+                    args[k] = other
+                    other = None
+                    self.args = tuple(args)
+                    break
+            if other != None:
+                self.args = (*self.args,other )
+
         self = self.__call__()
         return self
+
+
+# pipe first argument in Pipe() call, second argument with left pipe
+p = Pipe(2) >> (lambda x,y:2*x + y)  << 3.
+print(p)
+
+# pipe second argument in Pipe() using a placeholder for first arg, first argument with left pipe
+_ = PlaceHolderArg()
+p = Pipe(_,2) >> (lambda x,y:2*x + y)  << 3.
+print(p)
+
+# more complicated:
+def f(a,b,c,d):
+    return a,b,c,d
+p = Pipe(_,2.,_,4.) >> f  << (1.,3.)
+print(p)
